1fac5f31e7c76192e48096dd73000ff5a7681e61
Endnes <sergeev.krut@gmail.com>
2022-12-18 02:31:58 +0300

    анная программа принимает на вход число N и на выходе показывает все целые числа от -N до N

---
+++ Lesson_1_exemple_004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Данная программа принимает на вход число N и на выходе показывает все целые числа от -N до N");
+Console.Write("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number < 0) {
+    Console.WriteLine("Вы ввели не положтельное число.");
+}
+else {
+    int i = -number;
+
+    while (i <= number)
+    {
+        Console.Write($"{i} ");
+        i++;
+    }
+}
+

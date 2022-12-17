58225b93fe409c2c3336d33f1024ed97704a6478
Endnes <sergeev.krut@gmail.com>
2022-12-18 01:42:20 +0300

    add lesson 1 exemple 002

---
+++ Lesson_1_exempl_002/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA, numberB;
+Console.WriteLine("Данная программа проверяет является ли второе число квадратом первого.");
+Console.Write("Введите число А: ");
+numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+numberB = int.Parse(Console.ReadLine());
+if ((numberA * numberA) == numberB){
+    Console.WriteLine($"Число {numberB} является квадратом числа {numberA}.");
+} else {
+    Console.WriteLine($"Число {numberB} НЕ является квадратом числа {numberA}.");
+}
+

c16e88ebbf47aa4c03c5016a6774d6d4192d876b
Endnes <sergeev.krut@gmail.com>
2022-12-18 03:18:49 +0300

    Данная программа выдает максимальное число из 3 чисел.

---
+++ Lesson_1_DZ_002/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Данная программа выдает максимальное число из 3 чисел.");
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число В: ");
+int numberB = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число С: ");
+int numberC = int.Parse(Console.ReadLine()!);
+int max = numberA;
+if (numberB > max){
+    max = numberB;
+    if (numberC > max){
+        Console.WriteLine($"Число {numberC} больще всех");
+    } else {
+        Console.WriteLine($"Число {numberB} больше всех");
+    }
+}  else {
+    Console.WriteLine($"Число {numberA} больше всех");
+}

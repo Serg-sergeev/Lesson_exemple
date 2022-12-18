8b2d0a7f876e319e4bf25fbfc71ddc52e14e2f6f
Endnes <sergeev.krut@gmail.com>
2022-12-18 03:00:57 +0300

    Данная программа определяет какое число из двух больше, а какое меньше.

---
+++ Lesson_1_DZ_001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Данная программа определяет какое число из двух больше, а какое меньше.");
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число В: ");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberA > numberB) {
+    Console.WriteLine($"Число {numberA} больше числа {numberB}");
+    Console.WriteLine($"max = {numberA}");
+    Console.WriteLine($"min = {numberB}");
+} else {
+    Console.WriteLine($"Число {numberB} больше числа {numberA}");
+    Console.WriteLine($"max = {numberB}");
+    Console.WriteLine($"min = {numberA}");    
+}

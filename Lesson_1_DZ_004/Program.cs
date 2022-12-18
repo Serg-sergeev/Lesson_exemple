616c7a5112056a929fc816dbff66136c203407ae
Endnes <sergeev.krut@gmail.com>
2022-12-18 03:32:45 +0300

    Данная программа принимает на вход число N, а на выходе показывает все четные числа от 1 до N

---
+++ Lesson_1_DZ_004/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Данная программа принимает на вход число N, а на выходе показывает все четные числа от 1 до N.");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0){
+    int i = 2;
+    while (i <= number){
+        Console.Write($"{i} ");
+        i += 2;
+    }
+} else {
+    int i = 2;
+    while (i < number){
+        Console.Write($"{i} ");
+        i += 2;
+    }
+}
